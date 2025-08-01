11d25615779823528924e66582e38496b4c76534
knot126 <cddepppp256@proton.me>
2025-08-01 22:23:14 +0000

    Remove outdated tool

--- tools/leaf2andrleaf.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-from pathlib import Path
-
-Path("../KnShim/jni/andrleaf.h").write_text(Path("leaf.h").read_text().replace("printf(", '__android_log_print(ANDROID_LOG_INFO, "leaflib", '))
